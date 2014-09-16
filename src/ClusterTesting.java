682bd27a3192d7833c7d4b76ca362e571a16c8c2
bmikolajczyk15@jcu.edu <bmikolajczyk15@jcu.edu@7a08d398-b851-220b-59cc-ccaaef96b72d>
2014-09-16 18:52:59 +0000

    Added functioning test cases to check the compatibleness of two lists of data

--- src/ClusterTesting.java
+++ src/ClusterTesting.java
@@ -12,9 +12,9 @@ public class ClusterTesting {
 		//Distance Ranker is tested
 		DistanceRanker r = new DistanceRanker();
 		RankedData data1 = new RankedData(new int[] {1,2,3,4,5});
-		RankedData data2 = new RankedData(new int[] {5,4,3,2,1});
-		assertEquals(10, r.getDistance(data1, data2));
-		assertEquals(10, r.getDistance(data2, data1));
+		RankedData completeList = new RankedData(new int[] {5,4,3,2,1});
+		assertEquals(10, r.getDistance(data1, completeList));
+		assertEquals(10, r.getDistance(completeList, data1));
 		
 	}
 	@Test
@@ -22,9 +22,9 @@ public class ClusterTesting {
 	{
 		DistanceRanker r = new DistanceRanker();
 		RankedData data1 = new RankedData(new int[] {-1,2,3,4,5});
-		RankedData data2 = new RankedData(new int[] {5,4,3,2,1});
-		assertEquals(11, r.getDistance(data1, data2));
-		assertEquals(11, r.getDistance(data2, data1));
+		RankedData completeList = new RankedData(new int[] {5,4,3,2,1});
+		assertEquals(11, r.getDistance(data1, completeList));
+		assertEquals(11, r.getDistance(completeList, data1));
 	}
 	
 	@Test
@@ -32,9 +32,9 @@ public class ClusterTesting {
 	{
 		DistanceRanker r = new DistanceRanker();
 		RankedData data1 = new RankedData(new int[] {5,4,3,2,1});
-		RankedData data2 = new RankedData(new int[] {-1,2,3,4,5});
-		assertEquals(11, r.getDistance(data1, data2));
-		assertEquals(11, r.getDistance(data2, data1));
+		RankedData completeList= new RankedData(new int[] {-1,2,3,4,5});
+		assertEquals(11, r.getDistance(data1, completeList));
+		assertEquals(11, r.getDistance(completeList, data1));
 	}
 	
 	@Test
@@ -42,18 +42,42 @@ public class ClusterTesting {
 	{
 		DistanceRanker r = new DistanceRanker();
 		RankedData data1 = new RankedData(new int[] {-1,-2,-3,-4,-5});
-		RankedData data2 = new RankedData(new int[] {-5,-4,-3,-2,-1});
-		assertEquals(10,r.getDistance(data1, data2));
-		assertEquals(10, r.getDistance(data2, data1));
+		RankedData completeList = new RankedData(new int[] {-5,-4,-3,-2,-1});
+		assertEquals(10,r.getDistance(data1, completeList));
+		assertEquals(10, r.getDistance(completeList, data1));
 	}
 	@Test
 	public void testFirstAllNegative()
 	{
 		DistanceRanker r = new DistanceRanker();
 		RankedData data1 = new RankedData(new int[] {-5,-4,-3,-2,-1});
-		RankedData data2 = new RankedData(new int[] {5,4,3,2,1});
-		assertEquals(25,r.getDistance(data1, data2));
-		assertEquals(25, r.getDistance(data2, data1));
+		RankedData completeList = new RankedData(new int[] {5,4,3,2,1});
+		assertEquals(25,r.getDistance(data1, completeList));
+		assertEquals(25, r.getDistance(completeList, data1));
+	}
+	
+	@Test
+	public void testCompatable2Partial()
+	{
+		RankedData completeList = new RankedData(new int[] {1,2,3,4,5});
+		RankedData data1 = new RankedData(new int[] {1,2,3});
+		assertEquals(false,data1.isCompatableWithList(completeList)); // data1 must always be compared to the complete list to function properly
+	}
+	
+	@Test
+	public void testCompatable1Partial()
+	{
+		RankedData data1 = new RankedData(new int[] {2,3,4});
+		RankedData completeList = new RankedData(new int[] {1,2,4,3,5});
+		assertEquals(false,data1.isCompatableWithList(completeList));
+	}
+	
+	@Test
+	public void testCompatable()
+	{
+		RankedData data1 = new RankedData(new int[] {2,3,4,5,1});
+		RankedData completeList = new RankedData(new int[] {1,2,4,3,5});
+		assertEquals(true,data1.isCompatableWithList(completeList));
 	}
 
 }
