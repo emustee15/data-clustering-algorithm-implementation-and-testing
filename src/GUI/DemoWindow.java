771ce2ae13c592142af0c14d0a5c0d126cb276b6
emustee15@jcu.edu <emustee15@jcu.edu@7a08d398-b851-220b-59cc-ccaaef96b72d>
2014-09-23 20:02:26 +0000

    Did some work on the demo example of using swt. Also tested out file opening, it works very well!

--- src/GUI/DemoWindow.java
+++ src/GUI/DemoWindow.java
@@ -3,27 +3,32 @@ import org.eclipse.swt.*;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.RowData;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 public class DemoWindow
 {
+	
+	static Label label;
 	public static void main(String[] args)
 	{
 		Display display = new Display();
 		Shell shell = new Shell(display);
 		shell.setText("This is a test!");
 		
-		
-		Label label = new Label(shell,SWT.BORDER);
+		createMenuBar(shell);
+
+		label = new Label(shell,SWT.BORDER);
 		label.setText("HI!");
+		label.setBounds(10, 10, 20, 20);
 		
-		Text text = new Text(shell,SWT.BORDER);
-		text.setLayoutData(new RowData(100, SWT.DEFAULT));
+		Text text = new Text(shell,SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
+		text.setBounds(200, 50, 200, 200);
 		Button ok = new Button(shell, SWT.PUSH);
 		ok.setBounds(100, 50, 100, 100);
 		ok.setText("OK");
@@ -49,5 +54,82 @@ public class DemoWindow
 			}
 		}
 		display.dispose();
+	}
+	
+	public static void createMenuBar(final Shell shell)
+	{
+		Menu menuBar = new Menu(shell,SWT.BAR);
+		MenuItem cascadeFileMenu = new MenuItem(menuBar, SWT.CASCADE);
+		
+		
+		cascadeFileMenu.setText("&File");
+		Menu fileMenu = new Menu(shell, SWT.DROP_DOWN);
+		MenuItem openFile = new MenuItem(fileMenu,SWT.PUSH);
+		openFile.setText("Open");
+		
+		openFile.addSelectionListener(new SelectionListener()
+		{
+			
+			@Override
+			public void widgetSelected(SelectionEvent arg0)
+			{
+				FileDialog dialog = new FileDialog(shell, SWT.OPEN);
+				dialog.setText("Open");
+				dialog.setFilterPath("C:/");
+				String[] filterExt = {"*.txt"};
+				dialog.setFilterExtensions(filterExt);
+				String selectedFile = dialog.open();
+				if (selectedFile!=null)
+				{
+					label.setText(selectedFile);
+					label.pack();
+				}
+				else
+				{
+					label.setText("No file selected");
+					label.pack();
+				}
+				
+				
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		cascadeFileMenu.setMenu(fileMenu);
+		MenuItem exitItem = new MenuItem(fileMenu,SWT.PUSH);
+		exitItem.setText("Exit");
+		final MenuItem checkItem = new MenuItem(fileMenu, SWT.CHECK);
+		checkItem.setText("Check Me ;)");
+		checkItem.addSelectionListener(new SelectionListener()
+		{
+			
+			@Override
+			public void widgetSelected(SelectionEvent arg0)
+			{
+				if (checkItem.getSelection())
+				{
+					checkItem.setText("Uncheck Me ;)");
+				}
+				else
+				{
+					checkItem.setText("Check Me ;)");
+				}
+				
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		shell.setMenuBar(menuBar);
 	}
 }
