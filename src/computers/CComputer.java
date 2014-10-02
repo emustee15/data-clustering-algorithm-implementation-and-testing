8c3ec727ee04fc2efa0012f68494556ea6d38358
bmikolajczyk15@jcu.edu <bmikolajczyk15@jcu.edu@7a08d398-b851-220b-59cc-ccaaef96b72d>
2014-10-02 00:58:59 +0000

    Implemented the CComputer class for calculating the C values

--- src/computers/CComputer.java
+++ src/computers/CComputer.java
@@ -3,14 +3,35 @@ package computers;
 import java.util.ArrayList;
 
 import main.QVector;
+import main.RankedData;
 
 public class CComputer
 {
 	ArrayList<Double> cVector;
+	QVector qVector;
+	int numberClusters, numRankings;
+	ArrayList<RankedData> piVector;
+	
+	
+	public CComputer(QVector qVector, ArrayList<RankedData> piVector, int numberClusters)
+	{
+		this.qVector = qVector;
+		this.numberClusters = numberClusters;
+		this.piVector = piVector;
+		this.numRankings = piVector.size();
+	}
 	
 	public void computeCVector()
 	{
-		
+		double sum = 0;
+		for (int iCluster = 0; iCluster < numberClusters ; iCluster++)
+		{
+			for (int jRanking = 0; jRanking < numRankings; jRanking++)
+			{
+				sum += qVector.get(iCluster, jRanking);
+			}
+			cVector.set(iCluster, (1.0/numRankings)*sum);
+		}
 	}
 	public ArrayList<Double> getCVector()
 	{
